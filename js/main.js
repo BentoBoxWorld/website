9aa9174219c5f359e47a837089d4c86a1e3734e6
mastercake10 <dev@spaceio.de>
2018-12-05 18:17:43 +0100

    initial test commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var URL_MODULES = "https://ci.codemc.org/job/BentoBoxWorld/api/json"
+
+//var addons = 
+
+function buildForm(){
+  fetch(URL_MODULES)
+  .then(res => res.json())
+  .then((out) => {
+    console.log('Checkout this JSON! ', out);
+})
+.catch(err => { throw err });
+}
+
+function createJar(){
+  alert("test");
+}
