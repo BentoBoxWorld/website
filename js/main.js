fd2fb35d113f7095f9d283ebd7191d936b083a7e
mastercake10 <dev@spaceio.de>
2018-12-05 18:22:32 +0100

    test

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var URL_MODULES = "https://ci.codemc.org/job/BentoBoxWorld/api/json"
 //var addons = 
 
 function buildForm(){
-  fetch(URL_MODULES)
+  fetch(URL_MODULES, {'mode': 'no-cors'})
   .then(res => res.json())
   .then((out) => {
     console.log('Checkout this JSON! ', out);
